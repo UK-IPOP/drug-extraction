519224ce3f9d73f9dbeb9fa346bf92d35dc67ce5
Nick Anthony <nanthony007@gmail.com>
2021-10-06 09:58:22 -0400

    Switch to yaml-based config file for models.

--- pkg/models.go
+++ pkg/models.go
@@ -1,33 +1,25 @@
 package pkg
 
 import (
-	"encoding/json"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"os"
 )
 
 type Drug struct {
-	Name        string   `json:"name"`
-	SearchTerms []string `json:"search_terms"`
-	Tags        []string `json:"tags"`
+	Name        string   `json:"name" yaml:"Name"`
+	SearchTerms []string `json:"search_terms" yaml:"Search Terms"`
+	Tags        []string `json:"tags" yaml:"Tags"`
 }
 
 type Drugs struct {
-	Drugs []Drug `json:"drugs"`
+	Drugs []Drug `json:"drugs" yaml:"Drugs"`
 }
 
 func (Drugs) LoadFromFile() Drugs {
 	// TODO: add in optional filepath param
 	var drugs Drugs
-	jsonFile, err := os.Open("./data/drug_classes.json")
-	Check(err)
-	defer func(jsonFile *os.File) {
-		err := jsonFile.Close()
-		Check(err)
-	}(jsonFile)
-	byteVal, err := ioutil.ReadAll(jsonFile)
-	Check(err)
-	unmarshalErr := json.Unmarshal(byteVal, &drugs)
+	content, _ := ioutil.ReadFile("./data/drug_info.yaml")
+	unmarshalErr := yaml.Unmarshal(content, &drugs)
 	Check(unmarshalErr)
 	return drugs
 }
