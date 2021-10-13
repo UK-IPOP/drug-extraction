a792ee023459e954c63b018b18b873eaf93ce076
Nick Anthony <nanthony007@gmail.com>
2021-10-13 11:59:45 -0400

    Adjust to more sensible default of --clean=true flag

--- cmd/pipeline.go
+++ cmd/pipeline.go
@@ -47,7 +47,7 @@ func init() {
 	rootCmd.AddCommand(pipelineCmd)
 
 	// not required flags with defaults
-	pipelineCmd.Flags().BoolVar(&cleanStatus, "clean", false, "Remove existing output files for a clean run")
+	pipelineCmd.Flags().BoolVar(&cleanStatus, "clean", true, "Remove existing output files for a clean run")
 	pipelineCmd.Flags().BoolVar(&strictStatus, "strict", false, "Whether to perform strict-matching")
 	pipelineCmd.Flags().BoolVar(&formatStatus, "format", false, "Whether to format the data")
 	pipelineCmd.Flags().StringVar(&formatType, "format-type", "csv", "The new data format")
