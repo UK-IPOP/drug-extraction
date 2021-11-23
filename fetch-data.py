2a1f4e0fa3b62dd2297779778aecf114f7de7a18
Nick Anthony <nanthony007@gmail.com>
2021-11-22 20:33:59 -0500

    Fix filepath.

--- fetch-data.py
+++ fetch-data.py
@@ -5,7 +5,7 @@ client = Socrata("datacatalog.cookcountyil.gov", None)
 results = client.get("cjeq-bs86", limit=100_000)  # id for case archives dataset
 
 # jsonlines 😃
-with open("perf-comp/data/records.jsonl", "w") as f:
+with open("data/records.jsonl", "w") as f:
     for result in results:
         json.dump(result, f)
         f.write("\n")
