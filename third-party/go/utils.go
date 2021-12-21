b835f3663f2973cb718e21825831487dfc733973
Nick Anthony <nanthony007@gmail.com>
2021-12-21 16:03:11 -0500

    Lowercase when stripping characters from text.

--- third-party/go/utils.go
+++ third-party/go/utils.go
@@ -77,10 +77,10 @@ func GetUserInput() (string, error) {
 // strip makes alpha num
 func strip(s string) string {
 	var result strings.Builder
-	for i := 0; i < len(s); i++ {
-		b := s[i]
+	text := strings.ToLower(s)
+	for i := 0; i < len(text); i++ {
+		b := text[i]
 		if ('a' <= b && b <= 'z') ||
-			('A' <= b && b <= 'Z') ||
 			('0' <= b && b <= '9') ||
 			b == ' ' {
 			result.WriteByte(b)
