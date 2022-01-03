38548bd49d70f4144dc86925a909fe5196828660
Nick Anthony <nanthony007@gmail.com>
2022-01-03 15:20:19 -0500

    Add preliminary script to fetch data from RxNorm API.

---
+++ fetch-drug-info.py
@@ -0,0 +1,33 @@
+import json
+import requests
+from rich import print, pretty
+
+pretty.install()
+
+payload = {
+    "classId": "N02A",
+    "relaSource": "ATC",
+}
+url = "https://rxnav.nlm.nih.gov/REST/rxclass/classMembers.json"
+
+response = requests.get(url, params=payload)
+print(response.status_code)
+
+data = response.json()
+print(data)
+
+scraped_data = []
+for drug_member in data["drugMemberGroup"]["drugMember"]:
+    rx_id = drug_member["minConcept"]["rxcui"]
+    name = drug_member["minConcept"]["name"]
+    scraped_data.append(
+        {
+            "rx_id": rx_id,
+            "name": name,
+        }
+    )
+
+with open("data/drugs.jsonl", "w") as f:
+    for drug in scraped_data:
+        json_line = json.dumps(drug) + "\n"
+        f.write(json_line)
