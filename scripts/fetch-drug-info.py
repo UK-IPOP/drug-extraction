460cc9fe2eb6c342202422858f30eaf99a4c83c7
Nick Anthony <nanthony007@gmail.com>
2022-01-10 10:20:02 -0500

    Add more drug classes.

--- scripts/fetch-drug-info.py
+++ scripts/fetch-drug-info.py
@@ -4,30 +4,49 @@ from rich import print, pretty
 
 pretty.install()
 
-payload = {
-    "classId": "N02A",
-    "relaSource": "ATC",
-}
+request_data = [
+    {
+        "classId": "D000700",
+        "relaSource": "MESH",
+        "category_name": "analgesics",
+    },
+    {
+        "classId": "D000777",
+        "relaSource": "MESH",
+        "category_name": "stimulants",
+    },
+]
+
 url = "https://rxnav.nlm.nih.gov/REST/rxclass/classMembers.json"
 
-response = requests.get(url, params=payload)
-print(response.status_code)
 
-data = response.json()
-print(data)
+def remove_old_data():
+    """Remove old data from output file."""
+    with open("data/input/drugs.jsonl", "w") as f:
+        f.truncate()
 
-scraped_data = []
-for drug_member in data["drugMemberGroup"]["drugMember"]:
-    rx_id = drug_member["minConcept"]["rxcui"]
-    name = drug_member["minConcept"]["name"]
-    scraped_data.append(
-        {
-            "rx_id": rx_id,
-            "name": name,
-        }
-    )
 
-with open("data/input/drugs.jsonl", "w") as f:
-    for drug in scraped_data:
-        json_line = json.dumps(drug) + "\n"
-        f.write(json_line)
+remove_old_data()
+
+for payload in request_data:
+    response = requests.get(url, params=payload)
+    print(f"{payload['category_name']} --- {response.status_code}")
+
+    data = response.json()
+
+    scraped_data = []
+    for drug_member in data["drugMemberGroup"]["drugMember"]:
+        rx_id = drug_member["minConcept"]["rxcui"]
+        name = drug_member["minConcept"]["name"]
+        scraped_data.append(
+            {
+                "rx_id": rx_id,
+                "name": name,
+                "category_name": payload["category_name"],
+            }
+        )
+
+    with open("data/input/drugs.jsonl", "a") as f:
+        for drug in scraped_data:
+            json_line = json.dumps(drug) + "\n"
+            f.write(json_line)
