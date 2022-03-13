e574275e96e69736f569d2f5896716101f55c9a5
Nick Anthony <nanthony007@gmail.com>
2022-03-12 19:34:56 -0500

    Switch to benchmarking vs test logs.

--- python-lang/tests/test_str_sim.py
+++ python-lang/tests/test_str_sim.py
@@ -18,35 +18,35 @@ s2 = "acloholism"
 iterations = 1_000_000
 
 
-def benchmark_levenshtein_distance(benchmark):
+def test_benchmark_levenshtein_distance(benchmark):
     def f():
         return Levenshtein().distance(s1, s2)
 
     result = benchmark.pedantic(f, iterations=iterations)
 
 
-def benchmark_damerau_distance(benchmark):
+def test_benchmark_damerau_distance(benchmark):
     def f():
         return Damerau().distance(s1, s2)
 
     result = benchmark.pedantic(f, iterations=iterations)
 
 
-def benchmark_jarowinkler_distance(benchmark):
+def test_benchmark_jarowinkler_distance(benchmark):
     def f():
         return JaroWinkler().distance(s1, s2)
 
     result = benchmark.pedantic(f, iterations=iterations)
 
 
-def benchmark_sorensen_dice_distance(benchmark):
+def test_benchmark_sorensen_dice_distance(benchmark):
     def f():
         return SorensenDice().distance(s1, s2)
 
     result = benchmark.pedantic(f, iterations=iterations)
 
 
-def benchmark_optimal_string_alignment_distance(benchmark):
+def test_benchmark_optimal_string_alignment_distance(benchmark):
     def f():
         return OptimalStringAlignment().distance(s1, s2)
 
