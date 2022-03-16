890369639ba8531de8307ec41959186a0327a784
Nick Anthony <nanthony007@gmail.com>
2022-03-16 17:01:00 -0400

    Add remove flags on containers.

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -6,11 +6,11 @@ docker build -t python-benchmarks -f python-lang/Dockerfile ./python-lang
 
 docker build -t rust-benchmarks -f rust-lang/Dockerfile ./rust-lang
 
-docker run go-benchmarks
+docker run --rm go-benchmarks
 
-docker run python-benchmarks
+docker run --rm python-benchmarks
 
-docker run rust-benchmarks
+docker run --rm rust-benchmarks
 
 docker cp rust-benchmarks:/rust-app/target/criterion/ ./rust-report
 
