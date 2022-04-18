d713b5ef116492bd5bb787e4531c3df5d454be26
Nick Anthony <nanthony007@gmail.com>
2022-04-18 15:48:35 -0400

    Solid breakpoint. Working simple string searcher.

--- core/src/models.rs
+++ core/src/models.rs
@@ -1,57 +1,44 @@
-use strsim::{damerau_levenshtein, jaro_winkler, levenshtein, osa_distance, sorensen_dice};
-
-struct Input {
-    word: String,
-    group: Option<String>,
-}
-
-impl Input {
-    fn search_word(&self) -> &str {
-        &self.word
-    }
-}
-
-pub enum MetricType {
-    Metric,
-    Similarity,
-}
-
-pub struct DistanceResult {
-    value: f64,
-}
-
-pub struct Metric {
-    name: String,
-}
-
-impl Metric {
-    // distance is quicker but will only give you
-    // edits or similarity
-    fn distance(&self, source: &str, target: &str) -> DistanceResult {
-        let value = damerau_levenshtein(source, target) as f64;
-        DistanceResult { value }
-    }
-    // search handles more than distance
-    // search will give you a FULL result
-}
-
-fn initialize_metric() -> Metric {
-    let m = Metric {
-        name: "damerau".to_string(),
-    };
-    m
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::models::initialize_metric;
-
-    #[test]
-    fn test_initialize_metric() {
-        initialize_metric();
-        assert_eq!(true, true)
-    }
-}
-
-
-
+// use strsim::{damerau_levenshtein, jaro_winkler, levenshtein, osa_distance, sorensen_dice};
+//
+// struct SimpleExtractor {
+//
+// };
+//
+// extractor = E()  // rust code
+// results = []
+// for line in reader.lines() { // client code
+//     result = extractor.extract(line) // rust code
+//     results.append(result)
+// }
+//
+// // rust
+// data = results.to_json()
+//
+// // client
+// data.to_file()
+//
+// // extractor initializer
+// // params:
+// // metric: choice
+// // limit: float (similarity) 0-1
+// // ! NO merge input file w/ output?
+// //  - there are other, better tools for this (excel/pandas)
+// // dense or wide-form?
+//
+// // extractor.extract main component
+// // runs the distance function from the specified initializer
+// // returns:
+// // - search term
+// // - matched term
+// // - distance/null
+// // - similarity score
+// // - record id
+// // OPTIONALLY if drug-search
+// //      - drug group/class
+// //        - rx_id
+// //          - other identifiers
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+// }
--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,63 +1,143 @@
-use std::str::SplitWhitespace;
-
-use std::error::Error;
+use std::collections::HashMap;
+use std::error;
+use std::fmt;
+use std::fmt::Display;
+use std::hash::Hash;
+use std::iter::{Filter, FlatMap};
+use std::str::{FromStr, SplitWhitespace};
 use strsim::{damerau_levenshtein, jaro_winkler, levenshtein, osa_distance, sorensen_dice};
 
 /// Will need to be modified/extended to account for drug tags
 /// This will be serialized directly into json so this should
 /// be our final data structure that we want in the output
 
-pub struct DistanceResult {
-    edits: Option<i32>,
-    similarity: f64,
-    search: SimpleSearcher,
-    matched: String,
-    algorithm: dyn Algorithm,
-}
+/// ValueError occurs when an invalid value was provided
+#[derive(Debug)]
+pub struct ValueError;
 
-pub trait Algorithm {
-    // distance should take self.search_term() and compare to x
-    fn distance(source: &str, target: &str) -> f64;
-}
-pub struct SimpleSearcher {
-    word: String,
-    group: Option<String>,
-    algorithm: dyn Algorithm,
-}
-
-pub trait Searcher {
-    fn search_term(&self) -> &str;
-    fn compare(&self, x: &str) -> f64;
-}
-
-impl Searcher for SimpleSearcher {
-    fn search_term(&self) -> &str {
-        self.word.as_str()
-    }
-    fn compare(&self, x: &str) -> f64 {
-        self.algorithm.distance(self.search_term(), x)
+impl Display for ValueError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Received an unexpected value")
     }
 }
 
-fn search(searcher: &dyn Searcher, text: &str) -> Vec<f64> {
-    let r = text
-        .trim()
-        .split_whitespace()
-        .map(|x| {
-            let res = searcher.compare(x);
-            hi()
-            res
-        })
-        .collect();
-    r
+impl error::Error for ValueError {}
+
+type Result<T> = std::result::Result<T, ValueError>;
+
+
+// TODO: these functions could probably be better implemented using a closure or something
+// since they currently take 2 function calls to execute
+fn my_damerau(a: &str, b: &str) -> f64 {
+    damerau_levenshtein(a, b) as f64
 }
 
-fn run() {
-    let s = SimpleSearcher {
-        word: "ih".to_string(),
-        group: None,
-    };
-    let res = search(&s, "nik is cool ih likes drugs");
-    println!("{:?}", res);
+fn my_leven(a: &str, b: &str) -> f64 {
+    levenshtein(a, b) as f64
 }
 
+fn my_osa(a: &str, b: &str) -> f64 {
+    osa_distance(a, b) as f64
+}
+
+
+pub fn initialize_distance(a: Algorithm) -> fn(&str, &str) -> f64 {
+    match a {
+        Algorithm::DAMERAU => {my_damerau}
+        Algorithm::LEVENSHTEIN => {my_leven}
+        Algorithm::JAROWINKLER => {jaro_winkler}
+        Algorithm::OSA => {my_osa}
+        Algorithm::SORENSENDICE => {sorensen_dice}
+    }
+}
+
+
+#[derive(Clone, Copy, Debug)]
+pub enum Algorithm {
+    DAMERAU,
+    LEVENSHTEIN,
+    JAROWINKLER,
+    OSA,
+    SORENSENDICE,
+}
+
+impl Algorithm {
+    fn is_edits(&self) -> bool {
+        match self {
+            Algorithm::OSA | Algorithm::DAMERAU | Algorithm::LEVENSHTEIN => {
+                true
+            },
+            Algorithm::JAROWINKLER | Algorithm::SORENSENDICE => {
+                false
+            }
+        }
+    }
+}
+
+impl FromStr for Algorithm {
+   type Err = ValueError;
+    /// Parses an Algorithm type from a string reference.
+    fn from_str(s: &str) -> Result<Algorithm> {
+        match s.to_uppercase().as_str() {
+            "L" => Ok(Algorithm::LEVENSHTEIN),
+            "D" => Ok(Algorithm::DAMERAU),
+            "O" => Ok(Algorithm::OSA),
+            "J" => Ok(Algorithm::JAROWINKLER),
+            "S" => Ok(Algorithm::SORENSENDICE),
+            _ => Err(ValueError),
+        }
+    }
+}
+
+impl ToString for Algorithm {
+    fn to_string(&self) -> String {
+        match self {
+            Algorithm::DAMERAU => String::from("Damerau"),
+            Algorithm::LEVENSHTEIN => String::from("Levenshtein"),
+            Algorithm::OSA => String::from("OSA"),
+            Algorithm::JAROWINKLER => String::from("JaroWinkler"),
+            Algorithm::SORENSENDICE => String::from("SorensenDice"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Output {
+    pub record_id: String,
+    pub search_term: String,
+    pub matched_term: String,
+    pub algorithm: Algorithm,
+    pub edits: Option<i32>,
+    pub similarity: f64,
+}
+
+
+pub fn scan(a: Algorithm, distance: fn(&str, &str) -> f64, text: &str, record: &str, target: &str, limit: Option<f64>) -> Vec<Output> {
+    let clean = text.replace(&['(', ')', ',', '\"', '.', ';', ':'][..], "").to_uppercase();
+    let words = clean.split_whitespace();
+    let mut results: Vec<Output> = Vec::new();
+    for word in words {
+        let d = distance(target, word);
+        let res = Output {
+            record_id: record.to_string(),
+            search_term: target.to_string(),
+            matched_term: word.to_string(),
+            algorithm: a,
+            edits: if a.is_edits() { Some(d as i32) } else { None },
+            similarity: if !a.is_edits() {d} else {1.0 - (d / (target.chars().count().max(word.chars().count()) as f64))}
+        };
+        results.push(res);
+    }
+    match limit {
+        Some(l) => {
+            let mut filt_results: Vec<Output> = Vec::new();
+            for r in results {
+                if r.similarity >= l {
+                    filt_results.push(r);
+                }
+            }
+            filt_results
+        },
+        None => results
+    }
+}
