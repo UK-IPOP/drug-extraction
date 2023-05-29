1a14a4e612c77b3464975b2f3162930a0af89576
Nick Anthony <nanthony007@gmail.com>
2023-05-29 16:32:37 -0700

    Update rust files to lib.rs format

---
+++ cli/src/lib.rs
@@ -0,0 +1,414 @@
+use color_eyre::{
+    eyre::{eyre, Context, ContextCompat},
+    Result,
+};
+use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
+
+use std::{collections::HashSet, fs::File, path::Path, time::Duration};
+
+use serde::{Deserialize, Serialize};
+
+use itertools::Itertools;
+
+/// Create a spinner with default style, takes a message
+fn initialize_spinner_style(msg: String) -> ProgressBar {
+    let pb = ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(100));
+    pb.with_style(
+        ProgressStyle::with_template("{spinner:.blue} {msg}")
+            .unwrap()
+            .tick_strings(&[
+                "▹▹▹▹▹",
+                "▸▹▹▹▹",
+                "▹▸▹▹▹",
+                "▹▹▸▹▹",
+                "▹▹▹▸▹",
+                "▹▹▹▹▸",
+                "▪▪▪▪▪",
+            ]),
+    )
+    .with_message(msg)
+}
+
+/// Struct to hold search term and metadata
+#[derive(Deserialize, Debug, Clone, Default, PartialEq)]
+pub struct SearchTerm {
+    /// The search term
+    pub term: String,
+    /// Optional metadata to be included in output
+    pub metadata: Option<String>,
+}
+
+/// Struct to hold search output
+#[derive(Serialize, Debug, Clone, PartialEq)]
+pub struct SearchOutput<'a> {
+    /// The row id of the matched record, either from specified column or line number
+    row_id: &'a str,
+    /// The search term that was matched
+    search_term: &'a str,
+    /// The matched term from the record
+    matched_term: &'a str,
+    /// The number of edits required to match the search term
+    edits: usize,
+    /// The similarity score between the search term and the matched term
+    similarity_score: f64,
+    /// The field that was searched
+    search_field: &'a str,
+    /// The metadata associated with the search term
+    metadata: &'a Option<String>,
+}
+
+/// Function to read in search terms from a csv file
+/// Performs cleaning of terms, ignoring metadata column
+pub fn read_terms_from_file<P: AsRef<Path>>(p: P) -> Result<Vec<SearchTerm>> {
+    let mut rdr = csv::Reader::from_path(p).wrap_err("Unable to read search terms file")?;
+    let mut records: Vec<SearchTerm> = Vec::new();
+    // since this skips headers, the first row is 0 but that references line 1
+    // TODO: TEST THIS!
+    for (i, row) in rdr
+        .deserialize()
+        .enumerate()
+        .progress_with(initialize_spinner_style(
+            "Loading Search Terms...".to_string(),
+        ))
+    {
+        let mut record: SearchTerm =
+            row.wrap_err(format!("Could not load search term from line: {}", i + 1))?;
+        record.term = clean_text(&record.term);
+        records.push(record);
+    }
+    records.sort_by_key(|x| x.term.split_ascii_whitespace().count());
+    Ok(records)
+}
+
+/// Function to remove non-alphanumeric characters from a string
+/// keeps hyphens due to their usage in abbreviations/medical terms.
+/// Also uppercase for standardization.
+/// Example:
+/// ```
+/// use drug_extraction_cli::clean_text;
+///
+/// let s = "This is a test-string with 1234 and some punctuation!@#$%^&*()";
+/// let cleaned = clean_text(s);
+/// assert_eq!(cleaned, "THIS IS A TEST-STRING WITH 1234 AND SOME PUNCTUATION");
+/// ```
+pub fn clean_text(s: &str) -> String {
+    s.replace(|c: char| !c.is_ascii_alphanumeric() && c != '-', " ")
+        .trim()
+        .to_ascii_uppercase()
+}
+
+/// Struct to hold information about the dataset
+#[derive(Debug)]
+pub struct DataSet {
+    /// csv reader for the dataset
+    pub reader: csv::Reader<File>,
+    /// indices of the columns to search in the dataset
+    pub search_columns: Vec<ColumnInfo>,
+    /// index of the column to use as an id
+    pub id_column: Option<ColumnInfo>,
+    /// csv writer for the output file
+    pub writer: csv::Writer<File>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct ColumnInfo {
+    pub name: String,
+    pub index: usize,
+}
+
+/// Function to get the column index for a given column name
+/// Returns an error if the column name is not found
+/// Typically ran using the header from the csv reader and is called
+/// inside the [collect_column_info] function to do this for each target column.
+/// Example:
+/// ```
+/// use drug_extraction_cli::get_column_info;
+///
+/// let header = vec!["ID", "NAME", "DESCRIPTION"];
+/// let column = "NAME";
+/// let column_info = get_column_info(&header, &column);
+/// assert!(column_info.is_ok());
+/// let column_info = column_info.unwrap();
+/// assert_eq!(column_info.name, "NAME");
+/// assert_eq!(column_info.index, 1);
+/// ```
+pub fn get_column_info<S: AsRef<str> + PartialEq>(header: &[S], column: &S) -> Result<ColumnInfo> {
+    let pos = header.iter().position(|h| h == column);
+    match pos {
+        Some(i) => Ok(ColumnInfo {
+            name: column.as_ref().to_string(),
+            index: i,
+        }),
+        None => Err(eyre!("Unable to find column {}", column.as_ref())),
+    }
+}
+
+/// Function to collect column info for each column to search
+/// Typically ran using the header from the csv reader and is called
+/// inside the [initialize_dataset] function to do this for each target column.
+/// Example:
+/// ```
+/// use drug_extraction_cli::collect_column_info;
+///
+/// let header = vec!["ID", "NAME", "DESCRIPTION"];
+/// let columns = vec!["NAME", "DESCRIPTION"];
+/// let column_info = collect_column_info(&header, &columns);
+/// assert!(column_info.is_ok());
+/// let column_info = column_info.unwrap();
+/// assert_eq!(column_info.len(), 2);
+/// assert_eq!(column_info[0].name, "NAME");
+/// assert_eq!(column_info[0].index, 1);
+/// assert_eq!(column_info[1].name, "DESCRIPTION");
+/// assert_eq!(column_info[1].index, 2);
+/// ```
+pub fn collect_column_info<S: AsRef<str> + PartialEq>(
+    header: &[S],
+    column_names: &[S],
+) -> Result<Vec<ColumnInfo>> {
+    column_names
+        .iter()
+        .map(|column| get_column_info(header, column))
+        .collect()
+}
+
+/// Function to initialize the dataset
+pub fn initialize_dataset<P: AsRef<Path>>(
+    data_file: P,
+    search_columns: &[String],
+    id_column: Option<String>,
+) -> Result<DataSet> {
+    let mut rdr = csv::Reader::from_path(&data_file).wrap_err("Unable to initialize csv reader")?;
+    let header = rdr
+        .headers()
+        .wrap_err("Unable to parse csv headers")?
+        .into_iter()
+        .map(clean_text)
+        .collect_vec();
+    let column_info = collect_column_info(&header, search_columns)
+        .wrap_err("Unable to collect column indices")?;
+    Ok(match id_column {
+        Some(c) => DataSet {
+            reader: csv::Reader::from_path(&data_file)
+                .wrap_err("Unable to initialize csv reader")?,
+            search_columns: column_info,
+            id_column: Some(get_column_info(&header, &c)?),
+            writer: csv::Writer::from_path("output.csv")?,
+        },
+        None => DataSet {
+            reader: csv::Reader::from_path(&data_file)
+                .wrap_err("Unable to initialize csv reader")?,
+            search_columns: column_info,
+            id_column: None,
+            writer: csv::Writer::from_path("output.csv")?,
+        },
+    })
+}
+
+/// Primary search function
+pub fn search(mut dataset: DataSet, search_terms: Vec<SearchTerm>) -> Result<()> {
+    let mut total_records_with_matches = 0;
+    let mut total_records = 0;
+    let mut matched_terms: HashSet<&str> = HashSet::new();
+
+    let spinner = initialize_spinner_style("Searching for matches...".to_string());
+    for (i, row) in dataset
+        .reader
+        .records()
+        .enumerate()
+        .progress_with(spinner.clone())
+    {
+        let record = row.wrap_err(format!("Unable to read record from line {}", i))?;
+
+        let id = match &dataset.id_column {
+            Some(c) => record
+                .get(c.index)
+                .wrap_err(format!(
+                    "Unable to read id column {} from line {}",
+                    c.name, i
+                ))?
+                .to_string(),
+            None => i.to_string(),
+        };
+
+        let mut found_match = false;
+        for column in &dataset.search_columns {
+            let text = record.get(column.index).wrap_err(format!(
+                "Unable to read column {} from line {}",
+                column.name, i
+            ))?;
+            let cleaned_text = clean_text(text);
+            let grams = cleaned_text.split_ascii_whitespace().collect_vec();
+            for (term_len, term_list) in &search_terms
+                .iter()
+                .group_by(|st| st.term.split_ascii_whitespace().count())
+            {
+                let combos = if term_len == 1 {
+                    term_list.cartesian_product(
+                        grams
+                            .iter()
+                            .unique()
+                            .map(|word| word.to_string())
+                            .collect_vec(),
+                    )
+                } else {
+                    term_list.cartesian_product(
+                        grams
+                            .windows(term_len)
+                            .unique()
+                            .map(|words| words.join(" "))
+                            .collect_vec(),
+                    )
+                };
+                for (search_term, comparison_term) in combos {
+                    let edits = strsim::osa_distance(&search_term.term, &comparison_term);
+                    let sim = strsim::jaro_winkler(&search_term.term, &comparison_term);
+                    match edits {
+                        0 => {
+                            dataset
+                                .writer
+                                .serialize(SearchOutput {
+                                    row_id: &id,
+                                    search_term: &search_term.term,
+                                    matched_term: &comparison_term,
+                                    edits,
+                                    similarity_score: sim,
+                                    search_field: &column.name,
+                                    metadata: &search_term.metadata,
+                                })
+                                .wrap_err("Enable to serialize output")?;
+                            found_match = true;
+                            matched_terms.insert(&search_term.term);
+                        }
+                        1..=2 => {
+                            if sim >= 0.97 {
+                                dataset
+                                    .writer
+                                    .serialize(SearchOutput {
+                                        row_id: &id,
+                                        search_term: &search_term.term,
+                                        matched_term: &comparison_term,
+                                        edits,
+                                        similarity_score: sim,
+                                        search_field: &column.name,
+                                        metadata: &search_term.metadata,
+                                    })
+                                    .wrap_err("Enable to serialize output")?;
+                                found_match = true;
+                                matched_terms.insert(&search_term.term);
+                            }
+                        }
+                        _ => continue,
+                    }
+                }
+            }
+        }
+        if found_match {
+            total_records_with_matches += 1;
+        }
+        total_records += 1;
+    }
+    dataset.writer.flush().wrap_err("Unable to flush writer")?;
+    spinner.finish_with_message("Done!");
+
+    println!(
+        "Found matches in {:} of {:} records ({:.2}%)",
+        total_records_with_matches,
+        total_records,
+        (total_records_with_matches as f64 / total_records as f64) * 100.0
+    );
+    println!(
+        "Found {:} of {:} search terms ({:.2}%)",
+        matched_terms.len(),
+        search_terms.len(),
+        (matched_terms.len() as f64 / search_terms.len() as f64) * 100.0
+    );
+
+    Ok(())
+}
+
+pub fn run_searcher<P: AsRef<Path>>(
+    data_file: P,
+    search_terms_file: P,
+    search_columns: Vec<String>,
+    id_column: Option<String>,
+) -> Result<()> {
+    let search_terms = read_terms_from_file(search_terms_file)?;
+    let dataset = initialize_dataset(data_file, &search_columns, id_column)?;
+    search(dataset, search_terms)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_clean_text_no_changes() {
+        let s = "This is a test string.";
+        assert_eq!(clean_text(s), "this is a test string".to_ascii_uppercase());
+    }
+
+    #[test]
+    fn test_clean_text_numeric() {
+        let s = "This is a test string with 1234 numbers.";
+        assert_eq!(
+            clean_text(s),
+            "this is a test string with 1234 numbers".to_ascii_uppercase()
+        );
+    }
+
+    #[test]
+    fn test_clean_text_symbols() {
+        let s = "!@#$%^&*()_+-";
+        assert_eq!(clean_text(s), "-");
+    }
+
+    #[test]
+    fn test_clean_empty() {
+        let s = "";
+        assert_eq!(clean_text(s), "");
+    }
+
+    #[test]
+    fn test_clean_end_whitespace() {
+        let s = "!! This is a test string.   ";
+        assert_eq!(clean_text(s), "this is a test string".to_ascii_uppercase());
+    }
+
+    #[test]
+    fn test_get_column_info() {
+        let header = vec!["a", "b", "c"];
+        let col = "a";
+        assert_eq!(get_column_info(&header, &col).unwrap().index, 0);
+    }
+
+    #[test]
+    fn test_get_column_info_errors() {
+        let header = vec!["a", "b", "c"];
+        let col = "d";
+        assert!(get_column_info(&header, &col).is_err());
+    }
+
+    #[test]
+    fn test_collect_column_info() {
+        let header = vec!["a", "b", "c"];
+        let cols = vec!["a", "b"];
+        let info = collect_column_info(&header, &cols);
+        assert!(info.is_ok());
+        let info = info.unwrap();
+        assert_eq!(info.len(), 2);
+        assert_eq!(
+            info,
+            vec![
+                ColumnInfo {
+                    name: "a".to_string(),
+                    index: 0
+                },
+                ColumnInfo {
+                    name: "b".to_string(),
+                    index: 1
+                }
+            ]
+        );
+    }
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,57 +1,156 @@
-use std::io::{BufRead, BufReader};
+use clap::{Args, Parser, Subcommand};
+use color_eyre::{
+    eyre::{eyre, Context},
+    Result,
+};
+use dialoguer::{theme::ColorfulTheme, Confirm, Input, MultiSelect, Select};
+use drug_extraction_cli::run_searcher;
+use std::path::PathBuf;
 
-use clap::Parser; // this trait required for Cli::parse()
-use color_eyre::{eyre::eyre, Help, Result};
-use rayon::prelude::{ParallelBridge, ParallelIterator};
+fn welcome() {
+    println!("Welcome to the UK-IPOP Drug Extraction tool.");
+    println!("------------------------------------------");
+}
 
-mod in_and_out;
-mod options;
-mod searchers;
-mod types;
+#[derive(Parser, Debug)]
+#[command(
+    author,
+    about,
+    version,
+    long_about = "A fuzzy search tool for extracting drug mentions from large datasets."
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Interactive configuration wizard
+    Interactive,
+    /// File-IO
+    Search(SearchArgs),
+}
+
+#[derive(Args, Debug)]
+struct SearchArgs {
+    /// The file with your search terms
+    #[arg(short = 't', long, default_value = "search_terms.csv")]
+    terms_file: PathBuf,
+
+    /// The dataset file to search
+    #[arg(short = 'd', long)]
+    data_file: PathBuf,
+
+    /// The column name(s) in the dataset to search
+    #[arg(short = 'c', long, num_args = 1, required = true)]
+    search_cols: Vec<String>,
+
+    /// The column name in the dataset to keep as identifier [optional]
+    #[arg(short, long)]
+    id_col: Option<String>,
+}
+
+/// Interactive configuration wizard
+fn interactive_wizard() -> Result<SearchArgs> {
+    let theme = ColorfulTheme::default();
+
+    let terms_file: PathBuf = Input::<String>::with_theme(&theme)
+        .with_prompt("What is the path to the search terms file?")
+        .default("search_terms.csv".to_string())
+        .interact_text()?
+        .into();
+
+    // confirm that the file exists
+    if !terms_file.exists() {
+        return Err(eyre!("The file {} does not exist.", terms_file.display()));
+    }
+
+    let data_file: PathBuf = Input::<String>::with_theme(&theme)
+        .with_prompt("What is the path to the data file?")
+        .interact_text()?
+        .into();
+
+    // confirm that the file exists
+    if !data_file.exists() {
+        return Err(eyre!("The file {} does not exist.", data_file.display()));
+    }
+
+    let headers: Vec<String> = csv::Reader::from_path(&data_file)
+        .wrap_err("Could not open file")?
+        .headers()
+        .wrap_err("Could not read headers for datafile")?
+        .clone()
+        .iter()
+        .map(|h| h.to_ascii_uppercase())
+        .collect();
+
+    let search_cols = MultiSelect::with_theme(&theme)
+        .with_prompt(">Which column(s) do you want to search? \nMultiselect with <Space> and then <Enter> to continue.")
+        .items(&headers)
+        .interact()?;
+
+    if search_cols.is_empty() {
+        println!("You must select at least one column to search.");
+        println!("Use the arrow keys to select the columns you want to search.");
+        println!("Press `Space` to select and unselect columns and `Enter` to continue.");
+        std::process::exit(1);
+    }
+    // TODO: this seems backwards going from the index to the name...
+    let search_cols = search_cols
+        .iter()
+        .map(|&x| headers[x].to_string())
+        .collect::<Vec<String>>();
+
+    let has_id_col = Confirm::with_theme(&theme)
+        .with_prompt("Do you want to use an ID column?")
+        .default(false)
+        .interact()?;
+
+    let id_col = if has_id_col {
+        let id_col_index = Select::with_theme(&theme)
+            .with_prompt("Which column do you want to use as the ID column?")
+            .items(&headers)
+            .interact()?;
+        Some(headers[id_col_index].to_owned())
+    } else {
+        None
+    };
+
+    let args = SearchArgs {
+        terms_file,
+        data_file,
+        id_col,
+        search_cols,
+    };
+
+    Ok(args)
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let cli = options::Cli::parse();
-    // this is a great pattern since it validates CLI arguments
-    // FIRST before actually reading std-in
+    let cli = Cli::parse();
 
-    // pay attention since this is confusing and
-    // reverse of what it seems
-    if atty::isnt(atty::Stream::Stdin) {
-        // STD
-        println!("std");
-        dbg!(&cli);
-        match cli.command {
-            options::Commands::Pipe(args) => {
-                dbg!(&args);
-                searchers::pipe_searcher(args)?;
-            }
-            _ => {
-                return Err(
-                    eyre!("Piped-input only acceptable when using the `pipe` subcommand")
-                        .suggestion("Try running `fuzzy-drug-extractor pipe` with your piped data"),
-                )
-            }
-        };
-    } else {
-        // this is a great pattern because
-        // no-STD
-        println!("no std");
+    welcome();
 
-        dbg!(&cli);
-        match cli.command {
-            options::Commands::Interactive => println!("interactive search"),
-            options::Commands::Search(args) => {
-                dbg!(&args);
-                searchers::datafile_searcher(args)?;
-            }
-            _ => {
-                return Err(eyre!(
-                    "Non-piped-input only acceptable when using the `search` subcommand"
-                )
-                .suggestion("Try running `fuzzy-drug-extractor search` with your datafile"))
-            }
+    match cli.command {
+        Commands::Interactive => {
+            let args = interactive_wizard()?;
+            run_searcher(
+                args.data_file,
+                args.terms_file,
+                args.search_cols,
+                args.id_col,
+            )?;
+        }
+        Commands::Search(args) => {
+            run_searcher(
+                args.data_file,
+                args.terms_file,
+                args.search_cols,
+                args.id_col,
+            )?;
         }
     }
     Ok(())
